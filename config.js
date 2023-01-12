3296d138dc5365f0447ff56402860fea735e41c5
gongzhe01 <gongzhe01@longfor.com>
2023-01-12 23:00:37 +0800

    白名单增加吴博

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ export const botName = 'Jarvis'
 export const roomWhiteList = ['一顿狂式杵', '🎈都是高中惹的祸', '周一吃个饭', 'OpenAI-Group', '中老年活动中心', 'OpenAI-Inc', 'OpenAI-Beta', 'OpenAI-', '深夜澡堂🛁']
 
 // 联系人白名单，白名单内的联系人才会自动回复
-export const aliasWhiteList = ['G.z', 'Rella肉肉', 'ws', 'kedalgx', '喝水天天', '李广新', '谭蓉', '兔子']
+export const aliasWhiteList = ['G.z', 'Rella肉肉', 'ws', 'kedalgx', '喝水天天', '李广新', '谭蓉', '兔子', '吴博']
