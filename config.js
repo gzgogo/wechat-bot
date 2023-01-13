d3174491a7227083e3c5eaaccd4b1ad44583586f
gongzhe01 <gongzhe01@longfor.com>
2023-01-13 10:16:56 +0800

    支持AI-Jarvis

--- config.js
+++ config.js
@@ -1,8 +1,18 @@
 // 定义机器人的名称，这里是为了防止群聊消息太多，所以只有艾特机器人才会回复，
-export const botName = 'Jarvis'
+export const botName = 'Jarvis';
 
 // 群聊白名单，白名单内的群聊才会自动回复
-export const roomWhiteList = ['一顿狂式杵', '🎈都是高中惹的祸', '周一吃个饭', 'OpenAI-Group', '中老年活动中心', 'OpenAI-Inc', 'OpenAI-Beta', 'OpenAI-', '深夜澡堂🛁']
+export const roomWhiteList = [
+  '一顿狂式杵',
+  '🎈都是高中惹的祸',
+  '周一吃个饭',
+  'OpenAI-Group',
+  '中老年活动中心',
+  'OpenAI-Inc',
+  'OpenAI-Beta',
+  'OpenAI-',
+  '深夜澡堂🛁'
+];
 
 // 联系人白名单，白名单内的联系人才会自动回复
-export const aliasWhiteList = ['G.z', 'Rella肉肉', 'ws', 'kedalgx', '喝水天天', '李广新', '谭蓉', '兔子', '吴博']
+export const aliasWhiteList = ['G.z', 'Rella肉肉', 'ws', 'kedalgx', '喝水天天', '李广新', '谭蓉', '兔子', '吴博'];
--- src/wechaty/handleMessage.js
+++ src/wechaty/handleMessage.js
@@ -20,7 +20,7 @@ export async function handleMessage(msg, bot) {
   const remarkName = await contact.alias() // 备注名称
   const name = await contact.name() // 微信名称
   const isText = msg.type() === bot.Message.Type.Text // 消息类型是否为文本
-  const isRoom = roomName && (roomWhiteList.includes(roomName) || roomName.startsWith('OpenAI-')) // 是否在群聊白名单内
+  const isRoom = roomName && (roomWhiteList.includes(roomName) || roomName.startsWith('OpenAI-') || roomName.startsWith('深夜')) // 是否在群聊白名单内
   const isAlias = (remarkName && aliasWhiteList.includes(remarkName)) || aliasWhiteList.includes(name) // 发消息的人是否在联系人白名单内
   const isBotSelf = botName === remarkName || botName === name // 是否是机器人自己
   let isImage = false;
@@ -42,21 +42,21 @@ export async function handleMessage(msg, bot) {
       if (isRoom) {
         let quoteRegex = /^「[\s\S]*」\n- - - - - - - - - - - - - - -\n([\s\S]*)$/;
         if (quoteRegex.test(content)) {
-          let botQuoteRegex = /^「Jarvis：@.* ([\s\S]*)」\n- - - - - - - - - - - - - - -\n([\s\S]*)$/;
+          let botQuoteRegex = /^「(AI-)?Jarvis：@.* ([\s\S]*)」\n- - - - - - - - - - - - - - -\n([\s\S]*)$/;
           if (botQuoteRegex.test(content)) {
             let quoteMatch = botQuoteRegex.exec(content);
-            quote = quoteMatch[1];
+            quote = quoteMatch[2];
           }
           
           // 务必在获取quote之后执行，否则content内容被修改导致quote获取失败
           content = quoteRegex.exec(content)[1];
         };
       } else if (isAlias) {
-        let quoteRegex = /^「Jarvis：([\s\S]*)」\n- - - - - - - - - - - - - - -\n([\s\S]*)$/;
+        let quoteRegex = /^「(AI-)?Jarvis：([\s\S]*)」\n- - - - - - - - - - - - - - -\n([\s\S]*)$/;
         if (quoteRegex.test(content)) {
           let quoteMatch = quoteRegex.exec(content);
-          quote = quoteMatch[1];
-          content = quoteMatch[2];
+          quote = quoteMatch[2];
+          content = quoteMatch[3];
         };
       }
 
@@ -73,11 +73,13 @@ export async function handleMessage(msg, bot) {
 
       // 区分群聊和私聊
       // 群聊内引用时不需要@机器人，否则必须@机器人
-      if (isRoom && room && (quote || content.includes(`${botName}`))) {
+      if (isRoom && room && (quote || content.includes(`@${botName}`) || content.includes(`@AI-${botName}`))) {
         console.log(`\n--- ${name} in ${roomName}`);
 
         // 去掉@部分
-        content = content.replace(`@${botName}`, '').trim();
+        content = content.replace(`@AI-${botName}`, '')
+        content = content.replace(`@${botName}`, '');
+        content = content.trim();
 
         if (isImage) {
           let reply = await getImageReply(content);
