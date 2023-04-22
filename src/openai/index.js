21f50a0ae64181c22fedaf3949b1a1e7cd51f292
gongzhe01 <gongzhe01@longfor.com>
2023-04-23 00:18:06 +0800

    修复图片功能

--- src/openai/index.js
+++ src/openai/index.js
@@ -1,12 +1,12 @@
 import { remark } from 'remark'
 import stripMarkdown from 'strip-markdown'
-import axios from 'axios';
-import SocksProxyAgent from 'socks-proxy-agent';
-import HttpsProxyAgent from 'https-proxy-agent';
-import { Configuration, OpenAIApi } from 'openai';
-import dotenv from 'dotenv';
+import axios from 'axios'
+import SocksProxyAgent from 'socks-proxy-agent'
+import HttpsProxyAgent from 'https-proxy-agent'
+import { Configuration, OpenAIApi } from 'openai'
+import dotenv from 'dotenv'
 
-const env = dotenv.config().parsed; // 环境参数
+const env = dotenv.config().parsed // 环境参数
 
 // const httpsAgent = new HttpsProxyAgent('http://127.0.0.1:8001');
 const instance = axios.create({
@@ -15,9 +15,9 @@ const instance = axios.create({
   // proxy: false,
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${env.OPENAI_API_KEY}`,
-  }
-});
+    Authorization: `Bearer ${env.OPENAI_API_KEY}`,
+  },
+})
 
 // const instance = axios.create({
 //   timeout: 50000,
@@ -26,7 +26,7 @@ const instance = axios.create({
 // });
 
 export async function getChatReply(prompt) {
-  let reply = '';
+  let reply = ''
 
   try {
     console.log('🚀🚀🚀 / prompt: ', prompt)
@@ -34,23 +34,23 @@ export async function getChatReply(prompt) {
     const data = {
       model: 'gpt-3.5-turbo', // 'text-davinci-003',
       messages: [
-        { "role": "system", "content": "You are a helpful assistant." },
-        { "role": "user", "content": prompt }
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: prompt },
       ],
-      temperature: 0.2, // 每次返回的答案的相似度0-1（0：每次都一样，1：每次都不一样）0.9  
+      temperature: 0.2, // 每次返回的答案的相似度0-1（0：每次都一样，1：每次都不一样）0.9
       top_p: 1,
       max_tokens: 1024, // 回复字数限制，越大越慢
       frequency_penalty: 0.0, // 控制主题的重复度[-2.0, 2.0]
       presence_penalty: 0.0, // 控制主题的重复度[-2.0, 2.0] 正值会增加新话题的产生机率 default: 0.6
       // stop: [' Human:', ' AI:'],
-    };
+    }
 
-    const response = await instance.post('http://47.254.24.29/v1/chat/completions', data);
-    reply = response.data.choices[0].message.content;
+    const response = await instance.post('http://47.254.24.29/v1/chat/completions', data)
+    reply = response.data.choices[0].message.content
 
     // <br/>统一换成\n
-    reply.replace('<br/>', '\n');
-    reply.replace('<br />', '\n');
+    reply.replace('<br/>', '\n')
+    reply.replace('<br />', '\n')
 
     // 去掉开头的非字符内容
     reply = /^[\s,?!*#.。，？！、]*([\s\S]+)/.exec(reply)[1]
@@ -58,52 +58,9 @@ export async function getChatReply(prompt) {
     // const reply = markdownToText(response.data.choices[0].text)
     console.log('🚀🚀🚀 / reply: ', reply)
   } catch (error) {
-    reply = error.response ? `Error(${error.response.status}): ${error.response.statusText}` : `Error: ${error || '未知错误'}`;
-    console.log(error.response?.data.error.message);
-    console.error(error);
-  }
-
-  return reply;
-}
-
-const configuration = new Configuration({
-  apiKey: env.OPENAI_API_KEY,
-})
-const openai = new OpenAIApi(configuration)
-
-export async function getTextReply(prompt) {
-  let reply = '';
-
-  try {
-    console.log('🚀🚀🚀 / prompt: ', prompt)
-
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003', // 'text-davinci-003',
-      prompt: prompt,
-      temperature: 0.2, // 每次返回的答案的相似度0-1（0：每次都一样，1：每次都不一样）0.9  
-      top_p: 1,
-      max_tokens: 1000, // 回复字数限制，越大越慢
-      frequency_penalty: 0.0, // 控制主题的重复度[-2.0, 2.0]
-      presence_penalty: 0.0, // 控制主题的重复度[-2.0, 2.0] 正值会增加新话题的产生机率 default: 0.6
-      // stop: [' Human:', ' AI:'],
-    })
-
-    let choices = response.data.choices || [];
-    reply = choices[0].text || '';
-
-    // <br/>统一换成\n
-    reply.replace('<br/>', '\n');
-    reply.replace('<br />', '\n');
-
-    // 去掉开头的非字符内容
-    reply = /^[\s,?!*#.。，？！、]*([\s\S]+)/.exec(reply)[1]
-
-    // const reply = markdownToText(response.data.choices[0].text)
-    console.log('🚀🚀🚀 / reply: ', reply)
-  } catch (error) {
-    reply = error.response ? `Error(${error.response.status}): ${error.response.statusText}` : `Error: ${error || '未知错误'}`;
-    console.log(error.response?.data.error.message);
-    console.error(error);
+    reply = error.response ? `Error(${error.response.status}): ${error.response.statusText}` : `Error: ${error || '未知错误'}`
+    console.log(error.response?.data.error.message)
+    console.error(error)
   }
 
   return reply
@@ -112,22 +69,65 @@ export async function getTextReply(prompt) {
 export async function getImageReply(prompt) {
   try {
     console.log('🚀🚀🚀 / prompt: ', prompt)
-    const response = await openai.createImage({
-      model: "image-alpha-001",
+
+    const data = {
       prompt,
-    });
+      n: 1,
+      size: '1024x1024',
+    }
 
-    // console.log(response.data);
-
-    let reply = response.data.data[0].url;
+    const response = await instance.post('http://47.254.24.29/v1/images/generations', data)
+    let reply = response.data.data[0].url
     console.log('🚀🚀🚀 / reply: ', reply)
 
     return reply
   } catch (error) {
-    console.error(error);
-    return '';
+    console.error(error)
+    return ''
+  }
+}
+
+const configuration = new Configuration({
+  apiKey: env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
+
+export async function getTextReply(prompt) {
+  let reply = ''
+
+  try {
+    console.log('🚀🚀🚀 / prompt: ', prompt)
+
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003', // 'text-davinci-003',
+      prompt: prompt,
+      temperature: 0.2, // 每次返回的答案的相似度0-1（0：每次都一样，1：每次都不一样）0.9
+      top_p: 1,
+      max_tokens: 1000, // 回复字数限制，越大越慢
+      frequency_penalty: 0.0, // 控制主题的重复度[-2.0, 2.0]
+      presence_penalty: 0.0, // 控制主题的重复度[-2.0, 2.0] 正值会增加新话题的产生机率 default: 0.6
+      // stop: [' Human:', ' AI:'],
+    })
+
+    let choices = response.data.choices || []
+    reply = choices[0].text || ''
+
+    // <br/>统一换成\n
+    reply.replace('<br/>', '\n')
+    reply.replace('<br />', '\n')
+
+    // 去掉开头的非字符内容
+    reply = /^[\s,?!*#.。，？！、]*([\s\S]+)/.exec(reply)[1]
+
+    // const reply = markdownToText(response.data.choices[0].text)
+    console.log('🚀🚀🚀 / reply: ', reply)
+  } catch (error) {
+    reply = error.response ? `Error(${error.response.status}): ${error.response.statusText}` : `Error: ${error || '未知错误'}`
+    console.log(error.response?.data.error.message)
+    console.error(error)
   }
 
+  return reply
 }
 
 // function markdownToText(markdown) {
