77c14f01228c345fe78a2fa1c9e6cf4fbbef396e
gongzhen01 <gongzhen01@longfor.com>
2023-08-07 15:22:29 +0800

    更改微信群白名单

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ export const botName = '趣跨界智答'
 export const roomPrefix = '趣跨界'
 
 // 群聊白名单，白名单内的群聊才会自动回复
-export const roomWhiteList = ['趣跨界AI会员群', '趣跨界AI会员群01', '趣跨界AI会员群02']
+export const roomWhiteList = ['趣跨界智答会员群', '趣跨界智答会员群01', '趣跨界智答会员群02']
 
 // 私聊白名单，白名单内的联系人才会自动回复
 export const aliasWhiteList = ['林真实', 'G.z', '邵徽舜']
