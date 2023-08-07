00aad27556658883a61148b29a2ad1b051756b4a
gongzhen01 <gongzhen01@longfor.com>
2023-08-07 18:34:36 +0800

    更新广告语&群聊白名单

--- config.js
+++ config.js
@@ -5,7 +5,13 @@ export const botName = '趣跨界智答'
 export const roomPrefix = '趣跨界'
 
 // 群聊白名单，白名单内的群聊才会自动回复
-export const roomWhiteList = ['趣跨界智答会员群', '趣跨界智答会员群01', '趣跨界智答会员群02']
+export const roomWhiteList = [
+  '趣跨界商业合作群',
+  '趣开卷知识学习官方群',
+  '趣开卷＆袁燊老师国学学习交流群',
+  '趣开卷＆于希贤（北大教授）国学',
+  '趣开卷＆崔国文（清华教授）国学',
+]
 
 // 私聊白名单，白名单内的联系人才会自动回复
 export const aliasWhiteList = ['林真实', 'G.z', '邵徽舜']
